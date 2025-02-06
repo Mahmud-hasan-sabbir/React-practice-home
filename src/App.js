5ac85aa484ef9de9af01a58f93c37ec78584d464
Mahmud-hasan-sabbir <sabbirrahmanm1050@gmail.com>
2025-02-06 23:48:02 +0600

    today commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  var name = "Md. Shakil Ahmed";
+  var person = {'name': 'Shakil', 'job': 'Web Developer'};
+  var person2 = {name: 'Shakila', job: 'Web Developer'};
+  var style = {
+    color: 'red',
+    backgroundColor: 'yellow'
+  };
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mahmud hasan Sabbir
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My first react pragraph</p>
+        <h1>Headding line</h1>
+        <p>{(5+5) - 3}</p>
+        <h6 style={style}>Bangladesh top doctor : {name}</h6>
+        <h6>My name is : {person.name}</h6>
+        <h6>My job is : {person.job}</h6>
+        <p>person : {person2.name}</p>
+        <p>designation : {person2.job}</p>
+        <p style={style}>{person2.name + " " + person2.job}</p>
       </header>
     </div>
   );
